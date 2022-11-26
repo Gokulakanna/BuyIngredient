b64c6a9e045a9044157eccb3a89e8eaec6ecaf3c
Gokulakanna <gokulakannamsc@gmail.com>
2022-11-26 14:24:30 +0530

    Ingredient class added

--- BuyIngredient.java
+++ BuyIngredient.java
@@ -1,7 +1,23 @@
+Class Ingredient
+{
+	private int itemQuantity;
+
+	public void availQuantity()
+	{
+		System.out.println("---Available ingredient quantity---"+itemQuantity);
+	}
+	public void buyItem(int buyingQuantity)
+	{
+		this.itemQuantity=this.itemQuantity+buyingQuantity;
+	}
+}
 public class BuyIngredient
 {
 	public static void main(String[] args)
 	{
 		System.out.println("---Buying ingredients---");
+		Ingredient biscuit = new Ingredient();
+		biscuit.buyItem(10);
+		biscuit.availQuantity();
 	}
 }
